7167a6dba41a8e27a0d2481c05cdc03ba284adf4
Arun Shankar <arun@fuiszmedia.com>
2016-12-01 22:59:31 -0800

    added libs for identify and raw events

--- api/index.js
+++ api/index.js
@@ -1,5 +1,31 @@
-exports.identify = (req, res, next) => {
+
+exports.home = (req, res, next) => {
+  res.render('index', {title: 'Segment Nodejs'})
+}
+
+const lib = require('../lib')
+
+exports.raw = (req, res, next) => {
+  const json = req.body.json
+  console.log('the json is ', json)
   res.json({
-    segment: 'nodejs'
+    status: 'success'
   })
+}
+
+exports.identify = (req, res, next) => {
+  const data = req.body.json
+  lib.identify(data)
+    .then(data => {
+      res.json({
+        status: 'success',
+        message: data
+      })
+    })
+    .catch(err => {
+      res.json({
+        status: 'error',
+        message: err
+      })
+    })
 }
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,13 @@ var router = express.Router();
 
 var api = require('../api')
 /* GET home page. */
-router.post('/identify', (req, res, next) => api.identify(req, res, next))
+router.get('/', (req, res, next) => api.home(req, res, next))
+router.post('/api/raw', (req, res, next) => api.raw(req, res, next))
+router.post('/api/identify', (req, res, next) => api.identify(req, res, next))
+router.post('/api/track', (req, res, next) => api.track(req, res, next))
+router.post('/api/group', (req, res, next) => api.group(req, res, next))
+router.post('/api/page', (req, res, next) => api.page(req, res, next))
+router.post('/api/screen', (req, res, next) => api.screen(req, res, next))
+router.post('/api/alias', (req, res, next) => api.alias(req, res, next))
 
 module.exports = router;
